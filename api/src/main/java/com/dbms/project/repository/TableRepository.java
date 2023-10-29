a8c9cca0188ac7555e32435fd238ded1f7e52c4d
George-Iulian Danicico <72121511+GeorgeDanicico@users.noreply.github.com>
2023-10-29 13:59:32 +0200

    feature: implement select all rows (#4)

--- api/src/main/java/com/dbms/project/repository/TableRepository.java
+++ api/src/main/java/com/dbms/project/repository/TableRepository.java
@@ -2,11 +2,16 @@ package com.dbms.project.repository;
 
 import com.dbms.project.db.MongoUtils;
 import com.dbms.project.db.TableUtils;
+import com.dbms.project.exceptions.DBMSException;
 import com.dbms.project.model.Index;
 import com.dbms.project.model.PrimaryKey;
 import com.dbms.project.model.Table;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.model.Filters;
 import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
 import org.json.simple.JSONArray;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +19,9 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 //import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @SuppressWarnings("unchecked")
@@ -55,8 +63,39 @@ public class TableRepository {
         return TableUtils.getAllTablesForDatabase(databaseName);
     }
 
+    public List<?> getAllRows(String databaseName, Table table) throws Exception{
+        MongoTemplate mongoTemplate = mongoUtils.mongoTemplate(databaseName);
+        FindIterable<Document> documents = mongoTemplate.getCollection(table.getTableName()).find();
+
+        List<Map<String, String>> records = new ArrayList<>();
+        MongoCursor<Document> cursor = documents.iterator();
+        while (cursor.hasNext()) {
+            Map<String, String> rowValues = new HashMap<>();
+            List<Object> values = new ArrayList<>(cursor.next().values());
+            int i = 0;
+
+            String attributesValues = ((String) values.get(0) + ";") + ((String) values.get(1));
+            String[] attributes = attributesValues.split(";");
+            while (i < table.getAttributes().size()) {
+                rowValues.put(table.getAttributes().get(i).getAttributeName(), attributes[i]);
+                i ++;
+            }
+
+            records.add(rowValues);
+        }
+
+        return records;
+    }
+
     public void insertRow(String databaseName, String tableName, PrimaryKey key, String row) throws Exception{
         MongoTemplate mongoTemplate = mongoUtils.mongoTemplate(databaseName);
+        Bson filter = Filters.eq("_id", key.getPk());
+        FindIterable<Document> documents = mongoTemplate.getCollection(tableName).find(filter);
+        MongoCursor<Document> cursor = documents.iterator();
+        if (cursor.hasNext()) {
+            throw new DBMSException("Id already exists in table " + tableName + " from database " + databaseName);
+        }
+
         Document document = new Document("_id", key.getPk())
                 .append("value", row);
         mongoTemplate.getCollection(tableName).insertOne(document);
@@ -65,6 +104,5 @@ public class TableRepository {
     public void deleteRow(String databaseName, String tableName, String rowId) throws Exception{
         MongoTemplate mongoTemplate = mongoUtils.mongoTemplate(databaseName);
         mongoTemplate.getCollection(tableName).deleteOne(Filters.eq("_id", rowId));
-
     }
 }
