677aa08c21efdaa41e28d714a208d2c19b5130ff
GeorgeDanicico <georgedanicico@yahoo.com>
2023-11-11 17:38:49 +0200

    feature: implement indexes

--- api/src/main/java/com/dbms/project/repository/TableRepository.java
+++ api/src/main/java/com/dbms/project/repository/TableRepository.java
@@ -10,19 +10,18 @@ import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.model.Filters;
 import org.bson.Document;
-import org.bson.conversions.Bson;
 import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.util.Pair;
+//import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @SuppressWarnings("unchecked")
 @Repository
@@ -39,39 +38,110 @@ public class TableRepository {
         TableUtils.createTable(databaseName, table);
         MongoTemplate mongoTemplate = mongoUtils.mongoTemplate(databaseName);
         mongoTemplate.createCollection(table.getTableName());
-        table.getUniqueKeys().forEach(uniqueKey -> {
-            mongoTemplate.createCollection(table.getTableName() + "_" + uniqueKey);
-        });
+
+        for (var uniqueKey : table.getUniqueKeys()) {
+            var uniqueKeyIndexName = String.format("UK_%s_%s", table.getTableName(), uniqueKey);
+            mongoTemplate.createCollection(uniqueKeyIndexName);
+        }
+
+        for (var foreignKey : table.getForeignKeys()) {
+            var referencedAttributes = String.join("-", foreignKey.getReferencedAttributes());
+            var foreignKeyIndexName = String.format("FK_%s_%s", table.getTableName(), referencedAttributes);
+            mongoTemplate.createCollection(foreignKeyIndexName);
+        }
+
         return true;
     }
 
     public boolean deleteTable(String databaseName, String tableName) throws Exception{
         logger.info("Delete table: {} from database: {}", tableName, databaseName);
-        deleteUniqueAttributeTables(databaseName, tableName);
         TableUtils.deleteTable(databaseName, tableName);
         MongoTemplate mongoTemplate = mongoUtils.mongoTemplate(databaseName);
         mongoTemplate.dropCollection(tableName);
         return true;
     }
 
-    public void deleteUniqueAttributeTables(String databaseName, String tableName) throws Exception {
-        JSONObject tableJson = TableUtils.getTableFromDatabase(databaseName, tableName);
-        Table table = Table.fromJSON(tableJson);
+    public boolean deleteIndexTables(String databaseName, String tableName) throws Exception{
+        logger.info("Delete table: {} from database: {}", tableName, databaseName);
         MongoTemplate mongoTemplate = mongoUtils.mongoTemplate(databaseName);
-        table.getUniqueKeys().stream().map(attr -> tableName + "_" + attr).forEach(mongoTemplate::dropCollection);
+        mongoTemplate.dropCollection(tableName);
+        return true;
     }
 
     public boolean createIndex(String databaseName, String tableName, Index index) throws Exception {
         logger.info("Create index: {} for table: {} from database: {}", index.getIndexName(), tableName, databaseName);
         TableUtils.createIndex(databaseName, tableName, index);
+        var indexName = String.format("%s_%s_%s", index.getIsUnique() == 1 ? "UK" : "NUK", tableName, index.getIndexName());
+
         MongoTemplate mongoTemplate = mongoUtils.mongoTemplate(databaseName);
-//        mongoTemplate.getCollection(tableName).createIndex();
+        mongoTemplate.createCollection(indexName);
         return true;
     }
 
     public JSONArray getAllTables(String databaseName) throws Exception {
         logger.info("Get all tables called.");
         return TableUtils.getAllTablesForDatabase(databaseName);
+    }
+
+    public void insertRow(String databaseName, String tableName, PrimaryKey key, String row) throws Exception{
+        MongoTemplate mongoTemplate = mongoUtils.mongoTemplate(databaseName);
+        if (mongoTemplate.getCollection(tableName).find(Filters.eq("_id", key.getPk())).first() != null) {
+            throw new Exception("Duplicate primary key");
+        }
+        Document document = new Document("_id", key.getPk())
+                .append("value", row);
+        mongoTemplate.getCollection(tableName).insertOne(document);
+    }
+
+    public void deleteRow(String databaseName, String tableName, String rowId) {
+        try {
+            MongoTemplate mongoTemplate = mongoUtils.mongoTemplate(databaseName);
+            mongoTemplate.getCollection(tableName).deleteOne(Filters.eq("_id", rowId));
+        } catch (Exception e) {
+            throw new DBMSException("400", "Invalid row id");
+        }
+    }
+
+    public void insertRowInIndex(String databaseName, String indexName, PrimaryKey indexPrimaryKey, String pk, Boolean isUnique) throws Exception {
+        MongoTemplate mongoTemplate = mongoUtils.mongoTemplate(databaseName);
+        var index = mongoTemplate.getCollection(indexName).find(Filters.eq("_id", indexPrimaryKey.getPk())).first();
+
+        if (index == null) {
+            Document document = new Document("_id", indexPrimaryKey.getPk())
+                    .append("value", pk);
+            mongoTemplate.getCollection(indexName).insertOne(document);
+        } else {
+            if (isUnique) {
+                throw new DBMSException("400", "Violating Unique Key Constraint");
+            }
+            var newValue = index.get("value") + ";" + pk + ";";
+            mongoTemplate.getCollection(indexName).updateOne(Filters.eq("_id", indexPrimaryKey.getPk()), new Document("$set", new Document("value", newValue)));
+        }
+    }
+
+    public Map<String, String> getRow(String databaseName, Table table, String rowId) {
+        try {
+            MongoTemplate mongoTemplate = mongoUtils.mongoTemplate(databaseName);
+            var record = mongoTemplate.getCollection(table.getTableName()).find(Filters.eq("_id", rowId)).first();
+
+            Map<String, String> values = new HashMap<>();
+            String[] id = ((String) record.get("_id")).split(",");
+            String[] value = ((String) record.get("value")).split(";");
+            AtomicInteger index = new AtomicInteger();
+            AtomicInteger valueIndex = new AtomicInteger();
+
+            table.getPrimaryKey().getPkAttributes().forEach((attr) -> {
+                values.put(attr, id[index.getAndIncrement()]);
+            });
+            table.getAttributes().forEach((attr) -> {
+                if (!attr.getIsPrimaryKey()) {
+                    values.put(attr.getAttributeName(), value[valueIndex.getAndIncrement()]);
+                }
+            });
+            return values;
+        } catch (Exception e) {
+            throw new DBMSException("400", "Invalid row id");
+        }
     }
 
     public List<?> getAllRows(String databaseName, Table table) throws Exception{
@@ -98,41 +168,87 @@ public class TableRepository {
         return records;
     }
 
-    public void insertUniqueAttribute(String databaseName, String tableName, List<Pair<String, String>> uniqueAttributes, PrimaryKey key) throws Exception {
-        MongoTemplate mongoTemplate = mongoUtils.mongoTemplate(databaseName);
+    public void deleteRowForIndex(String databaseName, String indexName, String indexPk, String deletedPK, boolean isUnique) {
+        try {
+            if (isUnique) {
+                deleteRow(databaseName, indexName, indexPk);
+            } else {
+                MongoTemplate mongoTemplate = mongoUtils.mongoTemplate(databaseName);
+                var index = mongoTemplate.getCollection(indexName).find(Filters.eq("_id", indexPk)).first();
+                if (index == null) {
+                    throw new DBMSException("400", "Invalid foreign key value.");
+                }
+                var previousValues = ((String) index.get("value")).split(";");
+                StringBuilder newValue = new StringBuilder();
+                for (int i = 0; i < previousValues.length; i++) {
+                    if (previousValues[i].equals(deletedPK)) {
+                        continue;
+                    }
+                    if (previousValues.length > 2) {
+                        newValue.append(previousValues[i]).append(";");
+                    } else {
+                        newValue.append(previousValues[i]);
+                    }
+                }
+                if (newValue.isEmpty()) {
+                    deleteRow(databaseName, indexName, indexPk);
+                    return;
+                }
 
-        uniqueAttributes.forEach(uniqueAttr -> {
-            var tableUniqueName = tableName + "_" + uniqueAttr.getFirst();
-            Bson filter = Filters.eq("_id", uniqueAttr.getSecond());
-            FindIterable<Document> documents = mongoTemplate.getCollection(tableUniqueName).find(filter);
-            MongoCursor<Document> cursor = documents.iterator();
-            if (cursor.hasNext()) {
-                throw new DBMSException("Attribute " + uniqueAttr.getFirst() + " is unique and value " + uniqueAttr.getSecond() + " already exists in table " + tableName + " from database " + databaseName);
+                mongoTemplate.getCollection(indexName).updateOne(Filters.eq("_id", indexPk), new Document("$set", new Document("value", newValue.toString())));
             }
-            Document document = new Document("_id", uniqueAttr.getSecond())
-                    .append("value", key.getPk());
-            mongoTemplate.getCollection(tableUniqueName).insertOne(document);
-        });
-    }
-
-    public void insertRow(String databaseName, String tableName, PrimaryKey key, String row, List<Pair<String, String>> uniqueAttributes) throws Exception{
-        MongoTemplate mongoTemplate = mongoUtils.mongoTemplate(databaseName);
-        Bson filter = Filters.eq("_id", key.getPk());
-        FindIterable<Document> documents = mongoTemplate.getCollection(tableName).find(filter);
-        MongoCursor<Document> cursor = documents.iterator();
-        if (cursor.hasNext()) {
-            throw new DBMSException("Id already exists in table " + tableName + " from database " + databaseName);
+        } catch (Exception e) {
+            throw new DBMSException("400", "Invalid row id");
         }
-
-        insertUniqueAttribute(databaseName, tableName, uniqueAttributes, key);
-
-        Document document = new Document("_id", key.getPk())
-                .append("value", row);
-        mongoTemplate.getCollection(tableName).insertOne(document);
     }
 
-    public void deleteRow(String databaseName, String tableName, String rowId) throws Exception{
-        MongoTemplate mongoTemplate = mongoUtils.mongoTemplate(databaseName);
-        mongoTemplate.getCollection(tableName).deleteOne(Filters.eq("_id", rowId));
+
+
+    public void checkIfForeignKeyExists(String databaseName, String tableName, String rowId) {
+        try {
+            MongoTemplate mongoTemplate = mongoUtils.mongoTemplate(databaseName);
+            var index = mongoTemplate.getCollection(tableName).find(Filters.eq("_id", rowId)).first();
+            if (index == null) {
+                throw new DBMSException("400", "Invalid foreign key value.");
+            }
+        } catch (Exception e) {
+            throw new DBMSException("400", "Violated foreign key constraint.");
+        }
+    }
+
+    public void checkUniqueKey(String databaseName, String uniqueKeyIndexName, String key) {
+        try {
+            MongoTemplate mongoTemplate = mongoUtils.mongoTemplate(databaseName);
+            var index = mongoTemplate.getCollection(uniqueKeyIndexName).find(Filters.eq("_id", key)).first();
+            if (index != null) {
+                throw new DBMSException("400", "Violated unique key constraint.");
+            }
+        } catch (Exception e) {
+            throw new DBMSException("400", "Violated unique key constraint.");
+        }
+    }
+
+    public void checkUniqueIndex(String databaseName, String indexName, String pk) {
+        try {
+            MongoTemplate mongoTemplate = mongoUtils.mongoTemplate(databaseName);
+            var index = mongoTemplate.getCollection(indexName).find(Filters.eq("_id", pk)).first();
+            if (index != null) {
+                throw new DBMSException("400", "Violated unique index constraint.");
+            }
+        } catch (Exception e) {
+            throw new DBMSException("400", "Violated unique index constraint.");
+        }
+    }
+
+    public void checkIfForeignKeyReferenced(String databaseName, String tableName, String rowId) {
+        try {
+            MongoTemplate mongoTemplate = mongoUtils.mongoTemplate(databaseName);
+            var index = mongoTemplate.getCollection(tableName).find(Filters.eq("_id", rowId)).first();
+            if (index != null) {
+                throw new DBMSException("400", "Cannot delete. Row is referenced by another table.");
+            }
+        } catch (Exception e) {
+            throw new DBMSException("400", "Cannot delete. Row is referenced by another table.");
+        }
     }
 }
--- api/src/main/java/com/dbms/project/service/TableService.java
+++ api/src/main/java/com/dbms/project/service/TableService.java
@@ -10,12 +10,11 @@ import com.dbms.project.repository.TableRepository;
 import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Service
 public class TableService {
@@ -24,6 +23,15 @@ public class TableService {
 
     public TableService(TableRepository tableRepository) {
         this.tableRepository = tableRepository;
+    }
+
+    public void deleteTable(String databaseName, String tableName) throws Exception {
+        Table table = Table.fromJSON(TableUtils.getTableFromDatabase(databaseName, tableName));
+        deleteIndexTables(databaseName, table);
+        deleteUniqueKeysTables(databaseName, table);
+        deleteForeignKeyTables(databaseName, table);
+        checkIfTableIsReferenced(databaseName, tableName);
+        tableRepository.deleteTable(databaseName, tableName);
     }
 
     public void insertRow(String databaseName, String tableName, Map<String, String> values) {
@@ -43,6 +51,10 @@ public class TableService {
             }
         });
 
+        validateForeignKeyConstraint(databaseName, table, values);
+        validateUniqueKeys(databaseName, table, values);
+        validateIndexes(databaseName, table, values);
+
         String row = table.getAttributes().stream()
                 .filter(attr -> !attr.getIsPrimaryKey())
                 .map(attr -> {
@@ -50,20 +62,18 @@ public class TableService {
                     return values.get(attr.getAttributeName());
                 })
                 .reduce("", (result, name) -> result + name + ";");
+
         PrimaryKey primaryKey = new PrimaryKey(table.getAttributes().stream()
                 .filter(Attribute::getIsPrimaryKey)
                 .map(attr -> values.get(attr.getAttributeName()))
                 .toList());
-
-        var uniqueAttributes = table.getUniqueKeys().stream()
-                .map(attr -> Pair.of(attr, values.get(attr))).toList();
-
         try {
-            tableRepository.insertRow(databaseName, tableName, primaryKey, row, uniqueAttributes);
+            updateUniqueKeys(databaseName, table, primaryKey, values, "insert");
+            updateForeignKeys(databaseName, table, primaryKey, values, "insert");
+            updateIndexes(databaseName, table, primaryKey, values, "insert");
+            tableRepository.insertRow(databaseName, tableName, primaryKey, row);
         } catch (Exception e) {
-            if (e.getClass().equals(DBMSException.class)) {
-                throw new DBMSException(e.getMessage());
-            }
+            throw new DBMSException("400", e.getMessage());
         }
     }
 
@@ -74,13 +84,76 @@ public class TableService {
             throw new DBMSException("400", "Invalid table name. There is no table with this name.");
         }
         Table table = Table.fromJSON(tbl);
+        checkIfRowIsReferenced(databaseName, table, rowId);
+        Map<String, String> rowValues = tableRepository.getRow(databaseName, table, rowId);
+        PrimaryKey primaryKey = new PrimaryKey(table.getAttributes().stream()
+                .filter(Attribute::getIsPrimaryKey)
+                .map(attr -> rowValues.get(attr.getAttributeName()))
+                .toList());
 
         try {
+            updateUniqueKeys(databaseName, table, primaryKey, rowValues, "delete");
+            updateForeignKeys(databaseName, table, primaryKey, rowValues, "delete");
+            updateIndexes(databaseName, table, primaryKey, rowValues, "delete");
             tableRepository.deleteRow(databaseName, tableName, rowId);
         } catch (Exception e) {
 
+            throw new DBMSException("400", "Invalid row id");
         }
+    }
 
+    private void updateUniqueKeys(String databaseName, Table table, PrimaryKey primaryKey, Map<String, String> values, String operation) {
+        table.getUniqueKeys().forEach(key -> {
+            String uniqueKeyIndexName = String.format("UK_%s_%s", table.getTableName(), key);
+            try {
+                PrimaryKey indexPrimaryKey = new PrimaryKey();
+                indexPrimaryKey.setPkAttributes(new ArrayList<>(List.of(values.get(key))));
+                if (operation.equals("delete")) {
+                    tableRepository.deleteRowForIndex(databaseName, uniqueKeyIndexName, indexPrimaryKey.getPk(), null, true);
+                } else {
+                    tableRepository.insertRow(databaseName, uniqueKeyIndexName, indexPrimaryKey, primaryKey.getPk());
+                }
+            } catch (Exception e) {
+                logger.error("Error while updating unique key index: {}", uniqueKeyIndexName);
+                throw new DBMSException("400", "Violating Unique Key Constraint");
+            }
+        });
+    }
+
+    private void updateForeignKeys(String databaseName, Table table, PrimaryKey primaryKey, Map<String, String> values, String operation) {
+        table.getForeignKeys().forEach(key -> {
+            String foreignKeyIndexName = String.format("FK_%s_%s", table.getTableName(), String.join("-", key.getReferencedAttributes()));
+            try {
+                PrimaryKey indexPrimaryKey = new PrimaryKey();
+                key.getReferencedAttributes().forEach(attr -> indexPrimaryKey.setPkAttributes(new ArrayList<>(List.of(values.get(attr)))));
+                if (operation.equals("delete")) {
+                    tableRepository.deleteRowForIndex(databaseName, foreignKeyIndexName, indexPrimaryKey.getPk(), primaryKey.getPk(), false);
+                } else {
+                    tableRepository.insertRowInIndex(databaseName, foreignKeyIndexName, indexPrimaryKey, primaryKey.getPk(), false);
+                }
+            } catch (Exception e) {
+                logger.error("Error while updating foreign key index: {}", foreignKeyIndexName);
+                throw new DBMSException("400", "Invalid foreign key");
+            }
+        });
+    }
+
+    private void updateIndexes(String databaseName, Table table, PrimaryKey primaryKey, Map<String, String> values, String operation) {
+        table.getIndexes().forEach(index -> {
+            String indexName = String.format("%s_%s_%s", index.getIsUnique() == 1 ? "UK" : "NUK", table.getTableName(), index.getIndexName());
+            try {
+                PrimaryKey indexPrimaryKey = new PrimaryKey();
+                index.getIndexAttributes().forEach(attr -> indexPrimaryKey.setPkAttributes(new ArrayList<>(List.of(values.get(attr)))));
+                if (operation.equals("delete")) {
+                    tableRepository.deleteRowForIndex(databaseName, indexName, indexPrimaryKey.getPk(), primaryKey.getPk(), index.getIsUnique() == 1);
+                } else {
+                    tableRepository.insertRowInIndex(databaseName, indexName, indexPrimaryKey, primaryKey.getPk(), index.getIsUnique() == 1);
+                }
+            } catch (Exception e) {
+                logger.error("Error while updating  unique index: {}", indexName);
+                throw new DBMSException("400", "Invalid foreign key");
+            }
+        });
     }
 
     public List<?> getAllRows(String databaseName, String tableName) throws Exception {
@@ -88,5 +161,102 @@ public class TableService {
         Table table = Table.fromJSON(tbl);
         List<?> rows = tableRepository.getAllRows(databaseName, table);
         return rows;
+    }
+
+    private void deleteUniqueKeysTables(String databaseName, Table table) {
+        table.getUniqueKeys().forEach(key -> {
+            String uniqueKeyIndexName = String.format("UK_%s_%s", table.getTableName(), key);
+            try {
+                tableRepository.deleteIndexTables(databaseName, uniqueKeyIndexName);
+            } catch (Exception e) {
+                logger.error("Error while deleting unique key index: {}", uniqueKeyIndexName);
+                throw new DBMSException("400", "Invalid unique key");
+            }
+        });
+    }
+
+    private void deleteIndexTables(String databaseName, Table table) {
+        table.getIndexes().forEach(index -> {
+            String indexName = String.format("%s_%s_%s", index.getIsUnique() == 1 ? "UK" : "NUK", table.getTableName(), index.getIndexName());
+            try {
+                tableRepository.deleteIndexTables(databaseName, indexName);
+            } catch (Exception e) {
+                logger.error("Error while deleting index: {}", indexName);
+                throw new DBMSException("400", "Invalid index");
+            }
+        });
+    }
+
+    private void deleteForeignKeyTables(String databaseName, Table table) {
+        table.getForeignKeys().forEach(key -> {
+            String foreignKeyIndexName = String.format("FK_%s_%s", table.getTableName(), String.join("-", key.getReferencedAttributes()));
+            try {
+                tableRepository.deleteIndexTables(databaseName, foreignKeyIndexName);
+            } catch (Exception e) {
+                logger.error("Error while deleting foreign key index: {}", foreignKeyIndexName);
+                throw new DBMSException("400", "Invalid foreign key");
+            }
+        });
+    }
+
+    private void validateForeignKeyConstraint(String databaseName, Table table, Map<String, String> values) {
+        table.getForeignKeys().forEach(key -> {
+            var referencedTable = Table.fromJSON(TableUtils.getTableFromDatabase(databaseName, key.getReferencedTable()));
+            var referencedAttributes = key.getReferencedAttributes();
+            Validator.validateForeignKeyConstraint(referencedTable, referencedAttributes);
+            var referencedValues = String.join(",", key.getReferencedAttributes().stream()
+                    .map(values::get)
+                    .toList());
+            tableRepository.checkIfForeignKeyExists(databaseName, referencedTable.getTableName(), referencedValues);
+        });
+    }
+
+    private void validateUniqueKeys(String databaseName, Table table, Map<String, String> values) {
+        table.getUniqueKeys().forEach(key -> {
+            var uniqueKeyIndexName = String.format("UK_%s_%s", table.getTableName(), key);
+            tableRepository.checkUniqueKey(databaseName, uniqueKeyIndexName, key);
+        });
+    }
+
+    private void validateIndexes(String databaseName, Table table, Map<String, String> values) {
+        table.getIndexes().forEach(index -> {
+            if (index.getIsUnique() == 1) {
+                var indexName = String.format("%s_%s_%s", "UK", table.getTableName(), index.getIndexName());
+                PrimaryKey indexPrimaryKey = new PrimaryKey();
+                index.getIndexAttributes().forEach(attr -> indexPrimaryKey.setPkAttributes(new ArrayList<>(List.of(values.get(attr)))));
+                tableRepository.checkUniqueIndex(databaseName, indexName, indexPrimaryKey.getPk());
+            }
+        });
+    }
+
+    private void checkIfTableIsReferenced(String database, String tableName) {
+        var tables = TableUtils.getAllTablesForDatabase(database);
+        tables.forEach(tbl -> {
+            var table = Table.fromJSON((JSONObject) tbl);
+            table.getForeignKeys().forEach(key -> {
+                if (key.getReferencedTable().equals(tableName)) {
+                    logger.error("Table is referenced by another table");
+                    throw new DBMSException("400", "Table is referenced by another table");
+                }
+            });
+        });
+    }
+
+    private void checkIfRowIsReferenced(String databaseName, Table table, String rowId) {
+        var tables = TableUtils.getAllTablesForDatabase(databaseName);
+        tables.forEach(tbl -> {
+            var dbTable = Table.fromJSON((JSONObject) tbl);
+            dbTable.getForeignKeys().forEach(key -> {
+                if (key.getReferencedTable().equals(table.getTableName())) {
+                    var foreignKeyIndexName = String.format("FK_%s_%s", dbTable.getTableName(), String.join("-", key.getReferencedAttributes()));
+                    try {
+                        tableRepository.checkIfForeignKeyReferenced(databaseName, foreignKeyIndexName, rowId);
+                    } catch (Exception e) {
+                        logger.error("Error while checking if row is referenced: {}", foreignKeyIndexName);
+                        throw new DBMSException("400", e.getMessage());
+                    }
+                }
+            });
+        });
     }
 }
